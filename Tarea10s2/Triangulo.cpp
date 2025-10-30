ee5a8f1e8101293096fb21c3ebec1040706b9e84
lebrish-admin <cacerescamperonfabricio@gmail.com>
2025-10-29 23:28:23 -0500

    De

--- Tarea10s2/Triangulo.cpp
+++ Tarea10s2/Triangulo.cpp
@@ -8,5 +8,17 @@ void Triangulo::dibujar(Graphics^ gra) {
 	// Tener color
 	Color color = Color::FromArgb(r, g, b);
 	// Definir los puntos del triángulo
+	array<Point>^ puntos = gcnew array<Point>(3);
+	puntos[0] = Point(x + ancho / 2, y);           // Vértice superior (centro)
+	puntos[1] = Point(x, y + alto);                 // Vértice inferior izquierdo
+	puntos[2] = Point(x + ancho, y + alto);        // Vértice inferior derecho
 
+	// Llenar la figura de pintura/ dibujar relleno
+	SolidBrush^ broche = gcnew SolidBrush(color);
+	gra->FillPolygon(broche, puntos);
+	delete broche;
+	// Dibujar contorno
+	Pen^ lapiz = gcnew Pen(Color::Black, 3);
+	gra->DrawPolygon(lapiz, puntos);
+	delete lapiz;
 }
